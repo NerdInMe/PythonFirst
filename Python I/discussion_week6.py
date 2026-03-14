30384bdf6b5da34d5f40d02ec3217ac7a25d7ccd
NerdInMe <jainniyati1508@gmail.com>
2026-03-14 00:28:20 -0700

    Create discussion_week6.py

---
+++ Python I/discussion_week6.py
@@ -0,0 +1,15 @@
+### Discussion 6
+### Author: Niyati Jain
+
+#This weeks discussion is to understand the working of a string. There are several
+#things we can do with the string like search or replace a character. While doing the
+#palindrome checker assignment, I learnt about removing whitespace through replace method.
+#Now, understanding the replace function, I would replace the string as mentioned below.
+
+def replace():
+    input_str = "Hello World"
+    input_str = input_str.replace("World", "cuties <3")
+    print(input_str)
+
+if __name__ == "__main__":
+    replace()
